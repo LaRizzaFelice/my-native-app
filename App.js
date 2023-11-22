22866abff6cab14181800800365af96778f7d721
Felice La Rizza <105388204+LaRizzaFelice@users.noreply.github.com>
2023-11-22 21:07:48 +0100

    twrnc styled!

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { Platform, View } from "react-native";
-import { HomeScreen } from "./screens/HomeScreen";
+import { Banner, HomeScreen } from "./screens/HomeScreen";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
 function ProvidedApp() {
   return (
     <>
+      <Banner/>
       <HomeScreen/>
     </>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,33 @@
-import { StatusBar, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { StatusBar, Text } from "react-native";
+import tw from "twrnc";
+
+
+export function Banner() {
+  return (
+    <View style={styles.banner}>
+      <Text style={styles.bannerText}>React Native Exercices</Text>
+    </View>
+  );
+}
 
 export function HomeScreen() {
-
-    return (
-        <View>
-            <StatusBar></StatusBar>
-            <Text>Hello you</Text>
-        </View>
-    )
-
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  //banner: {
+  //backgroundColor: "purple",
+  //padding: 24,
+  //alignItems: "center",
+  //},
+  banner: tw`bg-purple-700 p-4`,
+  bannerText: tw`text-center text-white text-2xl`,
+  //bannerText: {
+  //fontSize: 24,
+  //},
+});
