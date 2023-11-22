b5adeb95c3ea55946f94071336dfabd2b6c94ec6
Felice La Rizza <105388204+LaRizzaFelice@users.noreply.github.com>
2023-11-22 19:37:18 +0100

    enkel werkend op android

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { HomeScreen } from "./screens/HomeScreen";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+function ProvidedApp() {
+  return (
+    <>
+      <HomeScreen/>
+    </>
+  );
+
+}
+
 
 export default function App() {
   return (
     <View>
-      <StatusBar style="auto"/>
-      <HomeScreen></HomeScreen>
+      <StatusBar />
+      <SafeAreaView>
+        <ProvidedApp />
+      </SafeAreaView>
     </View>
   );
 }
+
+
+
