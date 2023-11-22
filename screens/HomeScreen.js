3d96e654ebada61c17b5f421b607e010123f2d8e
Felice La Rizza <105388204+LaRizzaFelice@users.noreply.github.com>
2023-11-22 19:03:01 +0100

    zonder styling

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { StatusBar, Text, View } from "react-native";
+
+export function HomeScreen() {
+
+    return (
+        <View>
+            <StatusBar></StatusBar>
+            <Text>Hello you</Text>
+        </View>
+    )
+
+}
