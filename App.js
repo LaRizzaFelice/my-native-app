d0b9ae8b4ae0ab4bcc749929d77f4319212e1698
Felice La Rizza <105388204+LaRizzaFelice@users.noreply.github.com>
2023-11-29 19:59:37 +0100

    header nav styled

--- App.js
+++ App.js
@@ -5,15 +5,21 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { PlacesScreen } from "./screens/PlacesScreen";
 import { NAV_HOME, NAV_PLACES } from "./navigation_constants";
+import tw from "twrnc";
 
 
 
 const Stack = createNativeStackNavigator();
 
+const screenOptions = ({
+  headerStyle: tw`bg-red-700 p-4`,
+  headerTitleStyle: tw`text-center text-black text-lg`
+});
+
 function ProvidedApp() {
   return (
     <>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={screenOptions}>
         <Stack.Screen name={NAV_HOME} component={HomeScreen} />
         <Stack.Screen name={NAV_PLACES} component={PlacesScreen} />
       </Stack.Navigator>
